d587fffc8c19666c3e9fa98cd63327e867dfb4eb
DashaBaltsevich <dasha.baltsevich@mail.ru>
2022-03-23 04:18:38 -0700

    Send by post method
    1. Реализована отправка данных формы методом POST

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,20 +1,47 @@
 import ReactDOM from 'react-dom';
+import { useState } from 'react';
 import '../RegForm/RegForm.scss';
+import axios from 'axios';
 
 export const LoginForm = ({ setModalLogState }) => {
 
+    const [formLoginValues, setFormLoginValues] = useState({
+        email: '',
+        password: '',
+    })
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        console.log('Отправлено');
+        console.log(formLoginValues)
+
+        axios.post(``, formLoginValues)
+            .then((data) => {
+               console.log(data)
+            })
+            .catch(() => {
+              throw new Error('Error. No data');
+            })
+    }
+
     return ReactDOM.createPortal(
         <div className='form-reg__wrapper'>
-            <form className="form-reg"onSubmit={''}>                
-                <label for="email" className="label-reg">Почта:</label> 
-                <input type="email" id="email" onChange={''} className="input-reg"/>
+            <form className="form-reg" method="POST" onSubmit={handleFormSubmit}>                
+                <label htmlFor="email" className="label-reg">Почта:</label> 
+                <input type="email" id="email" value={formLoginValues.email} className="input-reg" onChange={(e) => setFormLoginValues(oldValues => ({
+                    ...oldValues,
+                    email: e.target.value
+                }))}/>
                 
-                <label for="password" className="label-reg">Пароль:</label>
-                <input type="password" id="password" onChange={''} className="input-reg"/>
+                <label htmlFor="password" className="label-reg">Пароль:</label>
+                <input type="password" id="password" value={formLoginValues.password} className="input-reg" onChange={(e) => setFormLoginValues(oldValues => ({
+                    ...oldValues,
+                    password: e.target.value
+                }))}/>
                 
-                <button className="btn-form-reg">Войти</button>
+                <button className="btn-form-reg" type="submit">Войти</button>
             </form>
-            <button className="btn-close" onClick={() => {
+            <button className="btn-close" type="submit" onClick={() => {
                 setModalLogState(false)
             }}>Закрыть</button>
         </div>,
--- src/components/RegForm/RegForm.js
+++ src/components/RegForm/RegForm.js
@@ -1,24 +1,59 @@
 import ReactDOM from 'react-dom';
+import { useState } from 'react';
+import axios from 'axios';
 import './RegForm.scss';
 
 
 
 export const RegForm = ({ setModalRegState }) => {
 
+    const [formRegValues, setFormRegValues] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+    })
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        console.log('Отправлено');
+        console.log(formRegValues)
+
+        axios.post(`/users`, formRegValues)
+            .then((data) => {
+               console.log(data)
+            })
+            .catch(() => {
+              throw new Error('Error. No data');
+            })
+    }
+
     return ReactDOM.createPortal(
         <div className='form-reg__wrapper'>
-            <form className="form-reg"onSubmit={''}>
-                <label for="firstName" className="label-reg">Имя:</label>
-                <input type="text" id="firstName" onChange={''} className="input-reg"/>
+            <form className="form-reg" method="POST" onSubmit={handleFormSubmit}>
+                <label htmlFor="firstName" className="label-reg">Имя:</label>
+                <input type="text" id="firstName" className="input-reg" value={formRegValues.firstName} onChange={(e) => setFormRegValues(oldValues => ({
+                    ...oldValues,
+                    firstName: e.target.value
+                }))}/>
                 
-                <label for="lastName" className="label-reg">Фамилия:</label>
-                <input type="text" id="lastName" onChange={''} className="input-reg"/>
+                <label htmlFor="lastName" className="label-reg">Фамилия:</label>
+                <input type="text" id="lastName" className="input-reg" value={formRegValues.lastName} onChange={(e) => setFormRegValues(oldValues => ({
+                    ...oldValues,
+                    lastName: e.target.value
+                }))}/>
                 
-                <label for="email" className="label-reg">Почта:</label> 
-                <input type="email" id="email" onChange={''} className="input-reg"/>
+                <label htmlFor="email" className="label-reg">Почта:</label> 
+                <input type="email" id="email" className="input-reg" value={formRegValues.email} onChange={(e) => setFormRegValues(oldValues => ({
+                    ...oldValues,
+                    email: e.target.value
+                }))}/>
                 
-                <label for="password" className="label-reg">Пароль:</label>
-                <input type="password" id="password" onChange={''} className="input-reg"/>
+                <label htmlFor="password" className="label-reg">Пароль:</label>
+                <input type="password" id="password" className="input-reg" value={formRegValues.password} onChange={(e) => setFormRegValues(oldValues => ({
+                    ...oldValues,
+                    password: e.target.value
+                }))}/>
                 
                 <button className="btn-form-reg">Зарегистрироваться</button>
             </form>
