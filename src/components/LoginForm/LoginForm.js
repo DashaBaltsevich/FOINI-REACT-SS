1a5e7e673fc69920207fd9184dfbf9f337f3c348
Yury Aliakseyeu <yury.aliakseyeu@dakrmatter.ae>
2022-05-10 14:25:58 +0300

    Add styles to formik errors

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -2,30 +2,28 @@ import { useContext } from 'react';
 import { AuthenticationContext } from '../../contexts';
 import { useAsync } from '../../hooks';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
+import * as yup from 'yup';
 import { Preloader } from '../Preloader';
 import { signIn } from '../../api/facades';
 import './LoginForm.scss';
+
+const validationSchema = yup.object({
+  email: yup
+    .string()
+    .email('Invalid email address')
+    .required('Email is required')
+    .max(30, 'Are you really sure that your email is that big?'),
+  password: yup
+    .string()
+    .required('Password is required')
+    .min(4, 'Must be more than 4 characters'),
+});
 
 export const LoginForm = ({ setIsLoginFormVisible }) => {
   const {
     actions: { setAuthState },
   } = useContext(AuthenticationContext);
   const { execute, loading } = useAsync(signIn, [], [], false);
-  const initialValues = {
-    email: '',
-    password: '',
-  };
-
-  const validationSchema = Yup.object({
-    email: Yup.string()
-      .email('Invalid email address')
-      .required('Email is required')
-      .max(30, 'Are you really sure that your email is that big?'),
-    password: Yup.string()
-      .required('Passowrd is required')
-      .min(4, 'Must be more than 4 characters'),
-  });
 
   const handleFormSubmit = async (values) => {
     try {
@@ -47,34 +45,54 @@ export const LoginForm = ({ setIsLoginFormVisible }) => {
       {loading && <Preloader />}
 
       <Formik
-        initialValues={initialValues}
+        initialValues={{
+          email: '',
+          password: '',
+        }}
+        validateOnBlur={false}
         validationSchema={validationSchema}
         onSubmit={(values) => handleFormSubmit(values)}
       >
         {({ values }) => (
           <Form className="f-login">
-            <label htmlFor="email" className="f-login__field-label">
-              Почта:
-            </label>
-            <Field
-              type="email"
-              id="email"
-              name="email"
-              className="f-login__field"
-              value={values.email}
-            />
-            <ErrorMessage name="password" />
+            <div className="f-login__row">
+              <label htmlFor="email" className="f-login__field-label">
+                Почта:
+              </label>
+              <br />
+              <Field
+                type="email"
+                id="email"
+                name="email"
+                className="f-login__field"
+                value={values.email}
+              />
+              <ErrorMessage
+                name="email"
+                component={({ children }) => (
+                  <p className="f-login__field-error">{children}</p>
+                )}
+              />
+            </div>
 
-            <label htmlFor="password" className="f-login__field-label">
-              Пароль:
-            </label>
-            <Field
-              type="password"
-              id="password"
-              name="password"
-              className="f-login__field"
-            />
-            <ErrorMessage name="password" />
+            <div className="f-login__row">
+              <label htmlFor="password" className="f-login__field-label">
+                Пароль:
+              </label>
+              <br />
+              <Field
+                type="password"
+                id="password"
+                name="password"
+                className="f-login__field"
+              />
+              <ErrorMessage
+                name="password"
+                component={({ children }) => (
+                  <p className="f-login__field-error">{children}</p>
+                )}
+              />
+            </div>
 
             <button type="submit" className="f-login__btn-submit">
               Войти
